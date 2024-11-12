69a38a90d4407ef2c44a033b10c51d0e81815988
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2024-11-12 16:58:59 +0200

    feat: add initial config for metadata links

--- fields.config.js
+++ fields.config.js
@@ -21,3 +21,30 @@ Registry.addMetadataField('contacts', {
     label: "Contacts",
     ext: "osc",
 });
+
+Registry.addMetadataField('osc:project', {
+  label: "Project",
+  ext: "osc",
+  formatter: value => Helper.toLink(`/stac-browser/#/projects/${value}/collection.json`, value)
+});
+
+Registry.addMetadataField('osc:themes', {
+  label: "Themes",
+  ext: "osc",
+  formatter: value =>
+    value.map(theme => Helper.toLink(`/stac-browser/#/themes/${theme}/catalog.json`, theme)).join(", ")
+});
+
+Registry.addMetadataField('osc:variables', {
+  label: "Variables",
+  ext: "osc",
+  formatter: value =>
+    value.map(variable => Helper.toLink(`/stac-browser/#/variables/${variable}/catalog.json`, variable)).join(", ")
+});
+
+Registry.addMetadataField('osc:missions', {
+  label: "Missions",
+  ext: "osc",
+  formatter: value =>
+    value.map(mission => Helper.toLink(`/stac-browser/#/eo-missions/${mission}/catalog.json`, mission)).join(", ")
+});
