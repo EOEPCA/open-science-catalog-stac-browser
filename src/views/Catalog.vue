370cae13b84c830732533e59eba56249aa831945
silvester-pari <silvester.pari@eox.at>
2026-05-20 23:38:49 +0200

    chore: revert catalog.vue more

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -11,6 +11,7 @@
             <Description :description="data.description" />
           </ReadMore>
           <Keywords v-if="Array.isArray(data.keywords) && data.keywords.length > 0" :keywords="data.keywords" class="mb-3" />
+          <CollectionLink v-if="collectionLink" :link="collectionLink" />
           <section v-if="isCollection" class="metadata mb-4">
             <b-row v-if="licenses">
               <b-col md="4" class="label">{{ $t('catalog.license') }}</b-col>
