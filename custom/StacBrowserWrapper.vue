0e9cf927e82bdde37611473a6181a77ccc949aa9
silvester-pari <silvester.pari@eox.at>
2026-06-24 15:29:03 +0200

    chore: v3.0.0-rc.4

--- custom/StacBrowserWrapper.vue
+++ custom/StacBrowserWrapper.vue
@@ -180,7 +180,7 @@ export default {
   },
   computed: {
     appVersion() {
-      return "3.0.0-rc.3";
+      return "3.0.0-rc.4";
     },
     apiAccessUrl() {
       const apiUrl = CONFIG.apiUrl || "https://eoapi.workspace.earthcode-staging.earthcode.eox.at/stac";
